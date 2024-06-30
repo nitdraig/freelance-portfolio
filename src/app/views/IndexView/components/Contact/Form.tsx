efcbe869f4ca4925b3fc4578a6dedce969e11650
Agustin-desktop <agustin2051@gmail.com>
2024-06-30 01:51:06 -0300

    form

--- src/app/views/IndexView/components/Contact/Form.tsx
+++ src/app/views/IndexView/components/Contact/Form.tsx
@@ -10,7 +10,7 @@ const Form = () => {
   const { language } = useLanguage();
   const webName = "Portfolio Freelance Landing";
   const emailDestiny = process.env.NEXT_PUBLIC_EMAIL_DESTINY || "";
-  const url = "https://mailprex-be-app.vercel.app/email/send";
+  const url = process.env.NEXT_PUBLIC_API_URL_SEND || "";
   const formToken = process.env.NEXT_PUBLIC_MAILPREX_FORM_TOKEN || "";
   const { formData, handleChange, handleSubmit, response } = useMailprex({
     url,
